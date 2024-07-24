ea8558f909fe02612f3ba9f27941817b03f91fce
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-07-24 12:05:44 +0300

    Fixed maven project setup. Deleted unused FXML package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,8 +8,9 @@ module org.example.javaharjoituksia {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.junit.jupiter.api;
-    requires org.junit.platform.commons;
     requires org.junit.platform.suite.api;
+    requires org.junit.platform.commons;
+    requires org.junit.jupiter.engine;
 
 }
 
