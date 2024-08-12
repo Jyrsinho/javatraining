4f1f265527899039e7966e5659a767e95dac029e
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-08-12 20:13:00 +0300

    Refactored GenericClassTest

--- src/test/java/GenericClass/GenericClassTest.java
+++ src/test/java/GenericClass/GenericClassTest.java
@@ -1,35 +1,43 @@
 package GenericClass;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GenericClassTest {
 
+    Printer printer;
+
+    @BeforeEach
+    void setUp() {
+        printer = new Printer();
+    }
+
     @Test
     public void testShouldPrintTheStringWhenGivenString() {
-        Printer printer = new Printer();
-
         assertEquals("Hello", printer.getDataAsString("Hello"));
 
     }
 
     @Test
     public void testShouldPrintTheIntegerWhenGivenInteger() {
-        Printer printer = new Printer();
         assertEquals("123", printer.getDataAsString("123"));
     }
 
     @Test
     public void testShouldDeclareStringAsStringWhenGivenString() {
 
-        Printer printer = new Printer();
         assertEquals( "String", printer.getDataAsClass("Hello"));
     }
 
     @Test
     public void testShouldDeclareIntegerAsIntegerWhenGivenInteger() {
-        Printer printer = new Printer();
         assertEquals( "Integer", printer.getDataAsClass(123));
+    }
+
+    @Test
+    public void testShouldReturnArray() {
+
     }
 }
