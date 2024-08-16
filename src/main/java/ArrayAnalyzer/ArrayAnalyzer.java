c0f93c2956d714071ebadda7192f8bb7820f2513
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-08-16 10:38:43 +0300

    Created a method for creating a histogram out of array in ArrayAnalyzer.

--- src/main/java/ArrayAnalyzer/ArrayAnalyzer.java
+++ src/main/java/ArrayAnalyzer/ArrayAnalyzer.java
@@ -9,11 +9,34 @@ public class ArrayAnalyzer {
     }
 
 
-    public int findMode() {
+    /**
+     * Creates a histogram from the array
+     * @return
+     */
+    public int[] createHistogram() {
+
+        int [] histogram = new int[10];
 
         for (int i = 0; i < t.length; i++) {
+
+        }
+        return histogram;
+    }
+
+
+    public int findMode() {
+
+        int []histogram = createHistogram();
+
+        int mode = 0;
+        int modevalue = histogram[0];
+
+        for (int i = 0; i < t.length; i++) {
+            if (histogram[i] < modevalue) {
+                mode = i;
+            }
         }
 
-        return 3;
+        return mode;
     }
 }
--- src/test/java/ArrayAnalyzerTest/ArrayAnazylerTest.java
+++ src/test/java/ArrayAnalyzerTest/ArrayAnazylerTest.java
@@ -8,6 +8,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ArrayAnazylerTest {
 
 
+
+    @Test
+    public void testHistogramShouldHaveLengthOfTen() {
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        ArrayAnalyzer arrayAnalyzer = new ArrayAnalyzer(array);
+
+        int [] histogram = arrayAnalyzer.createHistogram();
+        assertEquals(10, histogram.length);
+    }
+
+    @Test
+    public void testHistogramShouldHaveTwoInIndexFive() {
+        int[] array = {5,5};
+        ArrayAnalyzer arrayAnalyzer = new ArrayAnalyzer(array);
+
+        int [] histogram = arrayAnalyzer.createHistogram();
+
+        assertEquals(2,histogram[5]);
+
+    }
+
+    /*
     @Test
     public void testShouldReturnThreeFromArrayFullOfThrees() {
         int[] array = {3,3,3,3,3};
@@ -25,4 +47,14 @@ public class ArrayAnazylerTest {
         assertEquals(4, arrayAnalyzer.findMode());
 
     }
+
+    @Test
+    public void testShouldReturnFiveFromArrayFiveFiveTwo() {
+        int[] array = {2,5,5};
+        ArrayAnalyzer arrayAnalyzer = new ArrayAnalyzer(array);
+
+        assertEquals(5, arrayAnalyzer.findMode());
+    }
+
+     */
 }
