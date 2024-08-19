07853a64e727a65d2ea66068ee89db2199552d69
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-08-19 18:26:19 +0300

    CipherText encrypt and decrypt also works with random char arrays

--- src/main/java/CipherText/CipherText.java
+++ src/main/java/CipherText/CipherText.java
@@ -1,5 +1,9 @@
 package CipherText;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CipherText {
 
     private final char[] alphabet = {
@@ -45,29 +49,36 @@ public class CipherText {
 
 
     public char[]  createRandomAlphabet() {
-        char[] randomAlphabet = new char[alphabet.length];
 
-        for (int i = 0; i < alphabet.length; i++) {
-            randomAlphabet[i] = alphabet[i];
+       char [] randomAlphabet = new char[alphabet.length];
+       List<Character> alphabetList = new ArrayList<>();
+       for (char c : alphabet) {
+           alphabetList.add(c);
+       }
+
+        Collections.shuffle(alphabetList);
+
+        for (int i = 0; i < alphabetList.size(); i++) {
+            randomAlphabet[i] =alphabetList.get(i);
         }
-
         return randomAlphabet;
     }
+
 
 
     /**
      * Encrypts the plaintext attribute of the CipherText object
      * @return encrypted version of the plaintext
      */
-    public String encrypt() {
+    public String encrypt(String plainText, char[] alphabet) {
         StringBuilder cryptedText = new StringBuilder();
         int factor;                                     //determines whether we move forwards or backwards in alphabet array
         if (reversed) factor = -1;
         else factor = 1;
 
         for (int i = 0; i < plainText.length(); i++) {
-            int charactersIndex = findCharIndexFromAlphabet(plainText.charAt(i));
-            cryptedText.append(findCharacterToBeAppended(charactersIndex,factor,steps));
+            int charactersIndex = findCharIndexFromAlphabet(plainText.charAt(i), alphabet);
+            cryptedText.append(findCharacterToBeAppended(charactersIndex,factor,steps, alphabet));
         }
 
         this.cryptedText = cryptedText.toString();
@@ -75,24 +86,22 @@ public class CipherText {
     }
 
 
-    public String decrypt() {
+    public String decrypt(String cryptedText, char[] alphabet) {
         StringBuilder decryptedText = new StringBuilder();
         int factor;
         if (reversed) factor = 1;
         else factor = -1;
 
         for (int i = 0; i < cryptedText.length(); i++) {
-            int charactersIndex = findCharIndexFromAlphabet(cryptedText.charAt(i));
-            decryptedText.append(findCharacterToBeAppended(charactersIndex,factor,steps));
+            int charactersIndex = findCharIndexFromAlphabet(cryptedText.charAt(i), alphabet);
+            decryptedText.append(findCharacterToBeAppended(charactersIndex,factor,steps, alphabet));
         }
 
         return decryptedText.toString();
     }
 
 
-
-
-    public char findCharacterToBeAppended(int oCharacterIndex, int factor, int steps) {
+    public char findCharacterToBeAppended(int oCharacterIndex, int factor, int steps, char[] alphabet) {
         int newCharacterIndex;
 
         if (oCharacterIndex + factor * steps > alphabet.length-1 ||oCharacterIndex + factor * steps < 0 ){
@@ -109,12 +118,29 @@ public class CipherText {
      * @param c character to be found
      * @return the given characters index from the alphabet array
      */
-    public int findCharIndexFromAlphabet(char c) {
+    public int findCharIndexFromAlphabet(char c, char[] alphabet) {
         for (int i = 0; i < alphabet.length; i++) {
             if (c == alphabet[i]) {
                 return i;
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+
+        CipherText cipherText = new CipherText("ABBA",2, true);
+
+        for (int i = 0; i < cipherText.alphabet.length; i++) {
+            System.out.print(cipherText.alphabet[i]);
+        }
+
+        System.out.println();
+        char[] randomAlphabet = cipherText.createRandomAlphabet();
+
+        for (int i = 0; i < randomAlphabet.length; i++) {
+            System.out.print(randomAlphabet[i]);
+        }
+
     }
 }
--- src/test/java/CipherTextTest/CipherTextTest.java
+++ src/test/java/CipherTextTest/CipherTextTest.java
@@ -7,11 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CipherTextTest {
 
+    private final char[] alphabet = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z'
+    };
+
     @Test
     public void testCipherText() {
         CipherText cipherText = new CipherText("AUTO",1,false);
 
-        assertEquals("BVUP", cipherText.encrypt( ));
+        assertEquals("BVUP", cipherText.encrypt("AUTO", alphabet ));
 
     }
 
@@ -19,27 +25,27 @@ public class CipherTextTest {
     public void testCipherText2() {
         CipherText cipherText = new CipherText("BABA",1,false);
 
-        assertEquals("CBCB", cipherText.encrypt());
+        assertEquals("CBCB", cipherText.encrypt("BABA", alphabet ));
     }
 
     @Test
     public void testShouldReturnCipherTextWhenPlainTextCharsAtTheEndOfAlphabet() {
         CipherText cipherText = new CipherText("ZORRO",1,false);
 
-        assertEquals("APSSP", cipherText.encrypt());
+        assertEquals("APSSP", cipherText.encrypt("ZORRO", alphabet ));
     }
 
     @Test
     public void testShouldGiveProperCipherTextWhenThreeStepsAtTheEndOfAlphabet() {
         CipherText cipherText = new CipherText("ZYX",3,false);
 
-        assertEquals("CBA", cipherText.encrypt());
+        assertEquals("CBA", cipherText.encrypt("ZYX", alphabet ));
     }
 
     @Test
     public void testShouldGiveProperCipherTextWhenGoingBackWardsInAlphabet() {
         CipherText cipherText = new CipherText("ABBA",1, true);
-        assertEquals("ZAAZ", cipherText.encrypt());
+        assertEquals("ZAAZ", cipherText.encrypt("ABBA", alphabet ));
 
     }
 
@@ -47,14 +53,14 @@ public class CipherTextTest {
     public void testShouldGiveProperCipherTextWhenThreeStepsAtTheBeginningOfAlphabet() {
         CipherText cipherText = new CipherText("ABBA",3,true);
 
-        assertEquals("XYYX", cipherText.encrypt());
+        assertEquals("XYYX", cipherText.encrypt("ABBA", alphabet ));
     }
 
     @Test
     public void testShouldConvertCipherTextBackToPlaintext() {
         CipherText cipherText = new CipherText("ABBA",2,true);
-        cipherText.encrypt();
-        assertEquals("ABBA", cipherText.decrypt());
+        String cryptedText = cipherText.encrypt("ABBA", alphabet );
+        assertEquals("ABBA", cipherText.decrypt(cryptedText, alphabet));
     }
 
     @Test
@@ -74,6 +80,25 @@ public class CipherTextTest {
         char []randomAlphabet = cipherText.createRandomAlphabet();
 
         assertNotEquals('A', randomAlphabet[0]);
+    }
+
+    @Test
+    public void testShouldCreateRandomAlphabetWithUniqueCharacters() {
+        CipherText cipherText = new CipherText("ABBA",1,false);
+        char []randomAlphabet = cipherText.createRandomAlphabet();
+
+    }
+
+    @Test
+    public void testShouldBeAbleToEncryptAndDecryptWithRandomAlphabet() {
+
+        CipherText cipherText = new CipherText("ABBA",1,false);
+
+        char[] randomAlphabet = cipherText.createRandomAlphabet();
+
+        String cryptedText = cipherText.encrypt("ABBA", randomAlphabet);
+        assertEquals("ABBA", cipherText.decrypt(cryptedText, randomAlphabet));
+
     }
 
 }
