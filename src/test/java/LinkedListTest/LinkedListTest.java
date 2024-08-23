ae0415e8a9a718adc358925797e094751f877dcb
jyrihuhtala <jyrgenhuhtala@gmail.com>
2024-08-23 14:36:09 +0300

    Adding Nodes to the head of linkedlist works and added Nodes contain links to following nodes.

--- src/test/java/LinkedListTest/LinkedListTest.java
+++ src/test/java/LinkedListTest/LinkedListTest.java
@@ -2,19 +2,39 @@ package LinkedListTest;
 
 import LinkedList.LinkedList;
 import LinkedList.Node;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LinkedListTest {
 
+    LinkedList linkedList;
+
+    @BeforeEach
+    public void setUp() {
+        linkedList = new LinkedList();
+    }
+
     @Test
     public void testShouldAddNewHeadToLinkedList() {
-        LinkedList linkedList = new LinkedList();
-
         linkedList.addToHead("Hello1");
-
         assertEquals("Hello1",linkedList.getHead().getData());
+    }
+
+    @Test
+    public void testNewHeadShouldLinkToTheOldHead() {
+        linkedList.addToHead("Hello1");
+        linkedList.addToHead("Hello2");
+        linkedList.addToHead("Hello3");
+
+        Node head = linkedList.getHead();
+        Node second = linkedList.getHead().getNextNode();
+        Node third = second.getNextNode();
+
+        assertEquals("Hello2",second.getData());
+        assertEquals("Hello1",third.getData());
+
     }
 
 }
