e21dadac1716674422c669356f0573258b822034
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-09-06 18:34:03 +0300

    Created HashMap class and test for hash() function

---
+++ src/main/java/DataStructures/HashMap.java
@@ -0,0 +1,24 @@
+package DataStructures;
+
+public class HashMap {
+
+    public String[] hashmap;
+
+    public HashMap(int size) {
+        this.hashmap = new String[size];
+    }
+
+
+    public int hash(String key) {
+
+        return 10;
+    }
+
+    public int getSize() {
+        return hashmap.length;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/HashMapTest/HashMapTest.java
@@ -0,0 +1,23 @@
+package HashMapTest;
+
+import DataStructures.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HashMapTest {
+    HashMap hashMap;
+
+    @BeforeEach
+    void setUp() {
+        hashMap = new HashMap(5);
+    }
+
+    @Test
+    public void testShouldReturnIndexSuitableForArray() {
+        String testKey = "anneli";
+        int hash = hashMap.hash(testKey);
+        assertTrue(hash < hashMap.getSize() && hash >= 0);
+    }
+}
