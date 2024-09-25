ff11d2a36532ce07b83bf168741b00c8692fa4f4
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-09-25 18:54:07 +0300

    first collision handling test created

--- src/main/java/MyHashMap/MyHashMap.java
+++ src/main/java/MyHashMap/MyHashMap.java
@@ -1,5 +1,8 @@
 package MyHashMap;
 
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
 public class MyHashMap {
 
     private String[] hashmap;
@@ -8,12 +11,49 @@ public class MyHashMap {
         this.hashmap = new String[size];
     }
 
+    /**
+     * returns the index for a key
+     * @param key to be hashed
+     * @return the hashed index for key's value to be added in
+     */
     public int hash(String key) {
         int hashSum = 0;
         for (int i = 0; i < key.length() ; i++) {
             hashSum = hashSum + key.charAt(i);
         }
         return hashSum % hashmap.length;
+    }
+
+    /**
+     * Assigns value to a hashmap based on the hashing value of a given key
+     * @param key to be added
+     * @param value to be added
+     */
+    public void assign(String key, String value) {
+        int index = hash(key);
+        hashmap[index] = value;
+    }
+
+    public String getValue(String key) {
+        int index = hash(key);
+        if (hashmap[index] == null) {
+            throw new NoSuchElementException("No such key in hashmap");
+        }
+            return hashmap[index];
+    }
+
+    public String remove(String key) {
+        int index = hash(key);
+
+        if (hashmap[index] == null) {
+            throw new NoSuchElementException("No such key in hashmap");
+        }
+
+        String deleted = hashmap[index];
+        hashmap[index] = null;
+
+        return deleted;
+
     }
 
     public int getHashMapLenght() {
@@ -23,5 +63,7 @@ public class MyHashMap {
     public String[] getHashMap() {
         return this.hashmap;
     }
+
+    HashMap<String, Integer> myHashMap = new HashMap<>();
 
 }
--- src/test/java/HashMapTest/HashMapTest.java
+++ src/test/java/HashMapTest/HashMapTest.java
@@ -1,39 +1,120 @@
 package HashMapTest;
 
 import MyHashMap.MyHashMap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HashMapTest {
 
+    MyHashMap hashMap;
+
+    @BeforeEach
+    public void setUp() {
+        hashMap = new MyHashMap(10);
+    }
+
     @Test
     public void testConstructorShouldCreateHashMapOfGivenSize() {
-        MyHashMap hashMap = new MyHashMap(10);
         assertEquals(10, hashMap.getHashMapLenght());
     }
 
     @Test
     public void testHashShouldReturnValidHashingIndexForKey1() {
-        MyHashMap hashMap = new MyHashMap(10);
         assert(hashMap.hash("key1") >= 0 || hashMap.hash("key1") < hashMap.getHashMapLenght());
 
     }
 
     @Test
     public void testHashShouldReturnValidHashingIndexForKey2() {
-        MyHashMap hashMap = new MyHashMap(100);
+        MyHashMap hashMap2 = new MyHashMap(100);
         String key = "key2";
-        assert(hashMap.hash(key) >= 0 || hashMap.hash(key) < hashMap.getHashMapLenght());
+        assert(hashMap2.hash(key) >= 0 || hashMap2.hash(key) < hashMap2.getHashMapLenght());
     }
 
     @Test
     public void testShouldAssignValuesToHashMap() {
-        MyHashMap hashMap = new MyHashMap(10);
-        hashMap.assign("testKey", testValue);
+        hashMap.assign("testKey", "testValue");
         assertEquals(1, getAmountOfValuesInHashMap(hashMap.getHashMap()));
-
     }
+
+    @Test
+    public void testShouldAssignMultipleValuesToHashMap() {
+        hashMap.assign("testKey1", "testValue1");
+        hashMap.assign("testKey2", "testValue2");
+        hashMap.assign("testKey3", "testValue3");
+        assertEquals(3, getAmountOfValuesInHashMap(hashMap.getHashMap()));
+    }
+
+    @Test
+    public void testShouldGetValueFromHashMap() {
+        hashMap.assign("testKey1", "testValue1");
+        assertEquals("testValue1", hashMap.getValue("testKey1"));
+    }
+
+    @Test
+    public void testShouldGetValueFromHashMap2() {
+        hashMap.assign("testKey1", "testValue1");
+        hashMap.assign("testKey2", "testValue2");
+        hashMap.assign("testKey3", "testValue3");
+        assertEquals("testValue3", hashMap.getValue("testKey3"));
+    }
+
+    @Test
+    public void testShouldThrowNoSuchElementErrorWhenGettingValueFromHashMapThatDoesNotExist() {
+        hashMap.assign("testKey1", "testValue1");
+        hashMap.assign("testKey2", "testValue2");
+        hashMap.assign("testKey3", "testValue3");
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            hashMap.getValue("testKey4");
+        });
+        assertEquals("No such key in hashmap", exception.getMessage());
+    }
+
+
+    @Test
+    public void testShouldRemoveValueFromHashMap() {
+        hashMap.assign("testKey1", "testValue1");
+        hashMap.assign("testKey2", "testValue2");
+        hashMap.assign("testKey3", "testValue3");
+
+        String expected = "testValue1";
+        String actual = hashMap.remove("testKey1");
+
+        assertEquals(expected, actual);
+        assertEquals(2, getAmountOfValuesInHashMap(hashMap.getHashMap()));
+    }
+
+    @Test
+    public void testShouldThrowNoSuchElementErrorWhenRemovingValueFromHashMapThatDoesNotExist() {
+        hashMap.assign("testKey1", "testValue1");
+        hashMap.assign("testKey2", "testValue2");
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            hashMap.remove("testKey4");
+        });
+        assertEquals("No such key in hashmap", exception.getMessage());
+    }
+
+    @Test
+    public void testShouldAddTwoValuesWithSameHashToHashMap() {
+        hashMap.assign("testKey1", "testValue1");
+        hashMap.assign("testKey1", "testValue2");
+        assertEquals(2, getAmountOfValuesInHashMap(hashMap.getHashMap()));
+    }
+
+
+    /*
+     *   TODO - FOR COLLISION HANDLING LET'S DO TWO CLASSES THIS ONE DOES OPEN ADRESSING AND LLHASHMAP USES SEPARATE CHAINING WITH LINKED LISTS
+     *  TODO COLLISION HANDLING - open addressing, where a collision triggers a probing sequence to find where to store the value for a given key.
+     *   TODO COLLISION HANDLING - separate chaining, where each array index points to a different data structure
+     *
+     */
 
     public int getAmountOfValuesInHashMap(String[] hashMap) {
         int valuesInHashMap = 0;
