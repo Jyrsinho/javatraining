c6e42d19233d1821de172ffc79813b69bf32a00d
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-30 15:04:29 +0200

    Exercise for algorithm that finds out whether given String contains two same characters in a row

--- src/main/java/AlgorithmsOne/Algorithms.java
+++ src/main/java/AlgorithmsOne/Algorithms.java
@@ -4,13 +4,47 @@ public class Algorithms {
 
 
     // Algoritmi, joka laskee kahden kokonaisluvun välisen jakojäännöksen käyttämättä lainkaan jakolaskuja.
-    public static int modulo(int a, int b) {
-
-        /*
+     /*
         1. Otetaan kaksi kokonaislukua a ja b
-        2. Jos kokonaisluku a on pienempi kuin b
-         */
-        return 1;
+        2. Jos b on 0, palautetaan -1
+        3. Niin kauan kuin a on suurempi tai yhtäsuuri kuin b:
+            4 vähennetään b a:sta.
+        5. Palautetaan a.
+      */
+    public static int modulo(int a, int b) {
+        if (b == 0) {
+            return -1;
+        }
+        while (a >= b) {
+            a = a-b;
+        }
+        return a;
+    }
+
+
+   // algoritmi, joka tutkii, onko merkkijonossa jossain kohtaa kaksi samaa merkkiä peräkkäin.
+    /*
+        1. otetaan merkkijono s
+        2. asetetaan muuttuja n merkitsemään merkkien kokonaismäärää merkkijonossa s.
+        3, jos n:n arvo on pienempi kuin 2, palautetaan false.
+        4. asetetaan muuttujan a arvoksi 0 ja muuttujan b arvoksi 1.
+        5. niin kauan kuin muuttujan b arvo on pienempi kuin muuttujan n arvo:
+            6. tarkistetaan onko s:n indeksissä a sama merkki kuin s:n indeksissä b.
+                6.1 jos merkit ovat samat:
+                    6.1.1 palautetaan funktiosta true.
+                6.2 jos merkit ovat eri:
+                    6.2.1 lisätään a ja b muuttujien arvoja yhdellä.
+        7. palautetaan false.
+     */
+
+    public static boolean twoOfTheSame(String s) {
+        int n = s.length();
+        if (n < 2) return false;
+
+        int a = 0;
+        int b = 1;
+
+        return true;
     }
 
 }
--- src/test/java/AlgorithmsOneTest/AlgorithmsTest.java
+++ src/test/java/AlgorithmsOneTest/AlgorithmsTest.java
@@ -1,18 +1,12 @@
 package AlgorithmsOneTest;
 
-import AlgorithmsOne.Algorithms;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static AlgorithmsOne.Algorithms.modulo;
+import static AlgorithmsOne.Algorithms.twoOfTheSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlgorithmsTest {
-
-    @BeforeEach
-    public void setUp() {
-        Algorithms algorithmsOne = new Algorithms();
-    }
 
     @Test
     public void testShouldReturnModuloOfTwoIntegers() {
@@ -21,6 +15,59 @@ public class AlgorithmsTest {
         int expected = 1;
         assertEquals(modulo(a, b), expected);
 
+    }
+
+    @Test
+    public void testShouldReturnModuloOfTwoIntegers2() {
+        int a = 4;
+        int b = 2;
+        int expected = 0;
+        assertEquals(expected, modulo(a,b));
+    }
+
+    @Test
+    public void testShouldReturnModuloOfTwoIntegers3() {
+        int a = 0;
+        int b = 5;
+        int expected = 0;
+        assertEquals(expected, modulo(a,b));
+    }
+
+    @Test
+    public void testShouldReturnModuloOfTwoIntegers4() {
+        int a = 4;
+        int b = 0;
+        int expected = -1;
+        assertEquals(expected, modulo(a,b));
+    }
+
+    @Test
+    public void testShouldReturnModuloOfTwoIntegers5() {
+        int a = -5;
+        int b = 2;
+        int expected = 1;
+
+    }
+
+    @Test
+    public void testTwoOfTheSameShouldReturnFalseForStringsWithLessThanTwoChars() {
+        String s = "a";
+        boolean expected = false;
+        assertEquals(twoOfTheSame(s), expected);
+    }
+
+    @Test
+    public void testTwoOfTheSameShouldReturnTrueForAA() {
+        String s = "aa";
+        boolean expected = true;
+        assertEquals(twoOfTheSame(s), expected);
+    }
+
+    @Test
+    public void testTwoOfTheSameShouldReturnFalseForAB() {
+        String s = "ab";
+        boolean expected = false;
+        assertEquals(twoOfTheSame(s), expected);
     }
 
 }
