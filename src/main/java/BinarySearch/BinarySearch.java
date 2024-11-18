9b67c8d1a7974e145cfbc43f9179e9a4295d5b9a
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-11-18 15:05:15 +0100

    Tests for School

--- src/main/java/BinarySearch/BinarySearch.java
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -1,11 +1,40 @@
 package BinarySearch;
 
 import java.util.Arrays;
-
-
+import java.util.Random;
 
 
 public class BinarySearch {
+
+
+    public static class Result {
+
+        private int index;
+        private int numberOfComparisons;
+
+
+        /**
+         * Sets the index value of the Result object
+         *
+         * @param index where the integer is located in the array
+         */
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public void setNumberOfComparisons(int numberOfComparisons) {
+            this.numberOfComparisons = numberOfComparisons;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getNumberOfComparisons() {
+            return numberOfComparisons;
+        }
+
+    }
 
     /**
      * Returns the index of the searched integer using binary search
@@ -14,7 +43,7 @@ public class BinarySearch {
      * @return Result object that contains the index of the searched integer and
      * the amount of comparisons.
      */
-    public static Result binarySearch(int[] t, int target) {
+    public  Result binarySearch(int[] t, int target) {
         // Create new Result object to store the index of the searched integer and the number of comparisons
         // executed during the search
         Result result = new Result();
@@ -55,6 +84,57 @@ public class BinarySearch {
         result.setNumberOfComparisons(numberOfComparisons);
         result.setIndex(-1);
         return result;
+    }
+
+    /**
+     * Metodi, joka luo satunnaisia kokonaislukuja sisältävän taulukon
+     * @param size Taulukon koko
+     * @param min Pienin mahdollinen arvo
+     * @param max Suurin mahdollinen arvo
+     * @return Taulukko, joka sisältää satunnaisia kokonaislukuja
+     */
+    public static int[] generateSortedRandomArray(int size, int min, int max) {
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(max - min + 1) + min;
+        }
+
+        Arrays.sort(array);
+
+        return array;
+    }
+
+
+    public static void printArray(int [] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        /*
+        Luo satunnaisia kokonaislukuja sisältävä taulukko (esim. Random-luokan metodeilla) ja järjestä se käyttäen
+        jotakin valmista järjestysrutiinia (esim. Array.Sort). Generoitavat kokonaisluvut kannattaa valita joltakin
+        suhteellisen suppealta väliltä, esim. 0-50.
+Testeissä kannattaa käyttää sekä pieniä että suuria taulukoita. Pienet (noin 10 alkiota) voi tulostaa näytölle.
+Suurista taulukoista (esim 100 alkioita) kannattaa tulostaa vain esim. taulukon koko.
+Testitulostuksiin tuloksena tieto haettavan alkion löytymisestä ja vertailuoperaatioiden lukumäärä.
+         */
+
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println("Luodaan 10-alkioinen satunnainen järjestetty taulukko nollasta kymmeneen:");
+        int [] randomArray = generateSortedRandomArray(10, 0, 10);
+        printArray(randomArray);
+        System.out.println();
+        System.out.println("Etsitään taulukosta lukua 2");
+        Result result = binarySearch.binarySearch(randomArray, 2);
+        System.out.println("Luku 2 löytyi taulukosta indexistä:" +result.getIndex() + " Hakuun käytettiin " + result.getNumberOfComparisons() + " vertailua.");
+
+
     }
 
 }
--- src/test/java/BinarySearchTest/BinarySearchTest.java
+++ src/test/java/BinarySearchTest/BinarySearchTest.java
@@ -1,9 +1,9 @@
 package BinarySearchTest;
 
-import BinarySearch.Result;
+import BinarySearch.BinarySearch;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static BinarySearch.BinarySearch.binarySearch;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -13,12 +13,19 @@ public class BinarySearchTest {
 // ------------------------------------------------------------------------
 // TESTING THE BINARYSEARCH FUNCTION
 
+    BinarySearch bs;
+
+    @BeforeEach
+    public void setup() {
+        bs = new BinarySearch();
+    }
+
     @Test
     public void testShouldReturnZeroWhenSearchedIntegerIsTheOnlyIntegerInTheArray() {
         int target = 1;
         int[] array = {1};
         int expected = 0;
-        int result = binarySearch(array, target).getIndex();
+        int result = bs.binarySearch(array, target).getIndex();
         assertEquals(expected, result);
 
     }
@@ -28,7 +35,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3};
         int target = 2;
         int expected = 1;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
     }
@@ -38,7 +45,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3};
         int target = 1;
         int expected = 0;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
     }
@@ -48,7 +55,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3};
         int target = 3;
         int expected = 2;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
     }
@@ -58,7 +65,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3, 4};
         int target = 4;
         int expected = 3;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
     }
@@ -68,7 +75,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3, 4};
         int target = 1;
         int expected = 0;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
     }
@@ -78,7 +85,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3,4};
         int target = 2;
         int expected = 1;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
     }
@@ -88,7 +95,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3};
         int target = 4;
         int expected = -1;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
 
@@ -99,7 +106,7 @@ public class BinarySearchTest {
         int[] array = {};
         int target = 0;
         int expected = -1;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getIndex();
         assertEquals(expected, result);
 
@@ -111,7 +118,7 @@ public class BinarySearchTest {
         int[] array = {1, 2, 3, 4, 5};
         int target = 3;
         int expected = 1;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getNumberOfComparisons();
         assertEquals(expected, result);
     }
@@ -121,7 +128,7 @@ public class BinarySearchTest {
         int[] array = { 3, 5, 6, 7, 8, 9 };
         int target = 9;
         int expected = 3;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getNumberOfComparisons();
         assertEquals(expected, result);
     }
@@ -131,7 +138,7 @@ public class BinarySearchTest {
         int[] array = {};
         int target = 0;
         int expected = 0;
-        Result resultObject = binarySearch(array, target);
+        BinarySearch.Result resultObject = bs.binarySearch(array, target);
         int result = resultObject.getNumberOfComparisons();
         assertEquals(expected, result);
     }
