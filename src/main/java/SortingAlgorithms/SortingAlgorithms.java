760a7bc285313684ed932f0bdf91453f997add7c
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-12-05 15:57:10 +0200

    Done Bubble Sort

--- src/main/java/SortingAlgorithms/SortingAlgorithms.java
+++ src/main/java/SortingAlgorithms/SortingAlgorithms.java
@@ -12,7 +12,7 @@ public class SortingAlgorithms {
      * Method for printing all the elements in the array
      * @param array to be printed
      */
-    public void printArray(int[] array) {
+    public static void printArray(int[] array) {
         for (int element : array) {
             System.out.print(element + ", ");
         }
@@ -34,14 +34,70 @@ public class SortingAlgorithms {
         }
 
         return randomArray;
+    }
 
+
+    /**
+     * Sorts the given array using bubble sort.
+     * @param array to be sorted
+     * @param ascending if true, array will be sorted in ascending order, if false will be sorted in descending order.
+     * @return sorted array
+     */
+    public int[] bubbleSort(int[] array, boolean ascending) {
+        if (array.length < 2)
+            return array;
+
+        int sortedLimit = array.length - 1;
+
+        while (sortedLimit > 0) {
+            for (int i = 0, j = i + 1; i < sortedLimit; i++, j++) {
+                if (ascending) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+                }
+                else {
+                    if (array[i] < array[j]) {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+                }
+
+            sortedLimit -= 1;
+        }
+
+        return array;
+    }
+
+    /**
+     * Sorts the given array using Selection sort.
+     * @param array to be sorted
+     * @return array sorted
+     */
+    public int[] selectionSort(int[] array) {
+        return array;
     }
 
     public static void main (String[] args) {
-
+        int min = 0;
+        int max = 20;
+        int arraySize = 20;
         SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
-        int [] testArray = sortingAlgorithms.createRandomIntegerArray(100, 0, 100);
-        sortingAlgorithms.printArray(testArray);
+        int [] testArray = sortingAlgorithms.createRandomIntegerArray(arraySize, min, max);
+
+        System.out.println("Created unsorted random integer array of size  " + arraySize + " with min value of " + min + " and max value of " + max +": ");
+        printArray(testArray);
+        System.out.println();
+        System.out.println("Sorted the array with Bubble Sort in ascending order: ");
+        printArray(sortingAlgorithms.bubbleSort(testArray, true));
+        System.out.println();
+        System.out.println("Sorted the array with Bubble Sort in descending order: ");
+        printArray(sortingAlgorithms.bubbleSort(testArray, false));
+
     }
 
 }
--- src/test/java/SortingAlgorithmsTest/SortingAlgorithmsTest.java
+++ src/test/java/SortingAlgorithmsTest/SortingAlgorithmsTest.java
@@ -4,6 +4,7 @@ import SortingAlgorithms.SortingAlgorithms;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortingAlgorithmsTest {
@@ -22,5 +23,57 @@ public class SortingAlgorithmsTest {
         assertEquals(20, testAlgorithm.length);
 
     }
+
+    @Test
+    public void testBubbleSortShouldReturnAnIntegerArray() {
+        int[] testAlgorithm = {0};
+        int []expected = {0};
+        int[] result = sortingAlgorithms.bubbleSort(testAlgorithm, true);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testBubbleSortShouldSortArrayOfTwoIntegers() {
+        int[] testAlgorithm = {2,1};
+        int []expected = {1,2};
+        int[] result = sortingAlgorithms.bubbleSort(testAlgorithm, true);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testBubbleSortShouldSortArrayOfThreeIntegers() {
+        int[] testAlgorithm = {4, 3, 2,};
+        int []expected = {2,3,4};
+        int[] result = sortingAlgorithms.bubbleSort(testAlgorithm, true);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testBubbleSortShouldSortArrayOfFiveIntegersWithDuplicates() {
+        int[] testAlgorithm = {2, 3, 3, 2, 1};
+        int []expected = {1,2,2,3,3};
+        int[] result = sortingAlgorithms.bubbleSort(testAlgorithm, true);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testBubbleSortShouldSortArrayOfFiveIntegersToDescendingOrder() {
+        int[] testAlgorithm = {2,1,4,5,3};
+        int []expected = {5,4,3,2,1};
+        int[] result = sortingAlgorithms.bubbleSort(testAlgorithm, false);
+        assertArrayEquals(expected, result);
+    }
+
+    /*
+    @Test
+    public void testInsertionSortShouldReturnAnIntegerArray() {
+        int[] testAlgorithm = {0};
+        int []expected = {0};
+        int[] result = sortingAlgorithms.InsertionSort(testAlgorithm);
+        assertArrayEquals(expected, result);
+    }
+
+     */
+
 
 }
