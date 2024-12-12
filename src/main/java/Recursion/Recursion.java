8f7c23b5f6e502dc66d567c819f3276764636aef
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-12-12 13:56:05 +0200

    iterative fibonacci works

--- src/main/java/Recursion/Recursion.java
+++ src/main/java/Recursion/Recursion.java
@@ -3,8 +3,9 @@ package Recursion;
 public class Recursion {
 
     /**
-     * Counts the fibonacci number of given integer
+     * Counts the fibonacci number of given integer recursively
      * @param n given integer
+     * @return fibonacci number of given integer recursively
      */
         public static int recursiveFibonacci(int n) {
             if (n <= 1) {
@@ -12,6 +13,32 @@ public class Recursion {
             }
 
             return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+        }
+
+
+    /**
+     * Counts the fibonacci number of given integer iteratively
+     * @param n given integer
+     * @return fibonacci number of given integer recursively
+     */
+    public static int iterativeFibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        int i = 0;
+        int j = 1;
+        int loops = 1;
+        int fibonacci = 0;
+
+        while (loops < n) {
+            fibonacci = i + j;
+            i = j;
+            j = fibonacci;
+            loops++;
+        }
+        return fibonacci;
         }
 
 }
--- src/test/java/Recursion/TestRecursion.java
+++ src/test/java/Recursion/TestRecursion.java
@@ -2,6 +2,7 @@ package Recursion;
 
 import org.junit.jupiter.api.Test;
 
+import static Recursion.Recursion.iterativeFibonacci;
 import static Recursion.Recursion.recursiveFibonacci;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -53,6 +54,48 @@ public class TestRecursion {
     public void testRecursiveFibonacciShouldReturnFibonacciOfTen() {
         int expected = 55;
         int result = recursiveFibonacci(10);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIterativeFibonacciShouldReturnFibonacciOfZero() {
+        int expected = 0;
+        int result = iterativeFibonacci(0);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIterativeFibonacciShouldReturnOneForFibonacciOfOne() {
+        int expected = 1;
+        int result = iterativeFibonacci(1);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIterativeFibonacciShouldReturnOneForFibonacciOfTwo() {
+        int expected = 1;
+        int result = iterativeFibonacci(2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIterativeFibonacciShouldReturnFibonacciOfThree() {
+        int expected = 2;
+        int result = iterativeFibonacci(3);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIterativeFibonacciShouldReturnFibonacciOfFour() {
+        int expected = 3;
+        int result = iterativeFibonacci(4);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testIterativeFibonacciShouldReturnFibonacciOfTen() {
+        int expected = 55;
+        int result = iterativeFibonacci(10);
         assertEquals(expected, result);
     }
 }
