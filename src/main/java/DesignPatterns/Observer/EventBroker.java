071df1d8eea0095d8bee1366d9f24ff7a745c422
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-03-02 12:53:35 +0200

    EventBroker pattern

--- src/main/java/DesignPatterns/Observer/EventBroker.java
+++ src/main/java/DesignPatterns/Observer/EventBroker.java
@@ -1,14 +1,27 @@
 package DesignPatterns.Observer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class EventBroker {
 
-    HashMap<MyEvent, MyObserver> events;
+    HashMap<MyEvent, ArrayList<MyObserver>> events;
 
     public EventBroker() {
         events = new HashMap<>();
     }
 
+    /**
+     * Adds an observer to an event
+     * @param event that gets an observer
+     * @param observer that is added to an event
+     */
+    public void addObserver(MyEvent event, MyObserver observer) {
+        // if current event is already in the system, add new observer to it.
+        if (events.containsKey(event)) {
+            ArrayList<MyObserver> eventListerers = events.get(event);
+            eventListerers.add(observer);
+        }
+    }
 
 }
