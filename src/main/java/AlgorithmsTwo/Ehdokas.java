b613d7ebea828f46eca381876c61e6e16df10239
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-03-19 15:07:33 +0200

    Presidentinvaali

---
+++ src/main/java/AlgorithmsTwo/Ehdokas.java
@@ -0,0 +1,14 @@
+package AlgorithmsTwo;
+
+public class Ehdokas {
+
+    public String nimi;
+    public int aanimaara;
+
+
+    public Ehdokas(String nimi, int aanimaara) {
+        this.nimi = nimi;
+        this.aanimaara = aanimaara;
+    }
+
+}
