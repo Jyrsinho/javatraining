9d612ed5b1b5465bb79be0daeef0eca9ffbd7714
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-04-21 14:55:25 +0300

    Poikkeuusten tulostaminen skulaa, mutta paivamaaran tulostaminen tulee vaarassa muodossa

--- src/main/java/AlgorithmsTwo/LukuJarjestys/Poikkeustaja.java
+++ src/main/java/AlgorithmsTwo/LukuJarjestys/Poikkeustaja.java
@@ -1,6 +1,7 @@
 package AlgorithmsTwo.LukuJarjestys;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Poikkeustaja {
 
@@ -11,8 +12,16 @@ public class Poikkeustaja {
     }
 
     public ArrayList<Tapahtuma> annaPoikkeukset() {
-        ArrayList<Tapahtuma> poikkeukset = new ArrayList<>();
 
+        ArrayList<Tapahtuma> kaikkiPoikkeukset = haePoikkeukset();
+        ArrayList<Tapahtuma> uniikitPoikkeukset = poistaDuplikaatit(kaikkiPoikkeukset);
+        ArrayList<Tapahtuma> jarjestetytPoikkeukset = jarjestaPoikkeukset(uniikitPoikkeukset);
+        return uniikitPoikkeukset;
+    }
+
+
+    private ArrayList<Tapahtuma> haePoikkeukset() {
+        ArrayList<Tapahtuma> poikkeukset = new ArrayList<>();
         for (int i = 0; i <kalenteri.getTapahtumaKalenteri().length; i++) {
             for (int j = 0; j < kalenteri.getTapahtumaKalenteri()[i].length; j++) {
                 AikatauluRuutu tutkittava = kalenteri.getTapahtumaKalenteri()[i][j];
@@ -20,8 +29,27 @@ public class Poikkeustaja {
                 poikkeukset.addAll(aikatauluRuudunPoikkeukset);
             }
         }
-
-
         return poikkeukset;
     }
+
+    private ArrayList<Tapahtuma> poistaDuplikaatit(ArrayList<Tapahtuma> kaikkiPoikkeukset) {
+        ArrayList<Tapahtuma> uniikitPoikkeukset = new ArrayList<>();
+        for (Tapahtuma tapahtuma: kaikkiPoikkeukset) {
+            if (!uniikitPoikkeukset.contains(tapahtuma)) {
+                uniikitPoikkeukset.add(tapahtuma);
+            }
+        }
+        return uniikitPoikkeukset;
+    }
+
+    /**
+     *     jarjestaa Tapahtumat ensin paivamaaran ja sitten kellonajan mukaan
+     */
+    private ArrayList<Tapahtuma> jarjestaPoikkeukset(ArrayList<Tapahtuma> poikkeukset) {
+        poikkeukset.sort(Comparator.comparing(Tapahtuma::getPaivamaara));
+        return poikkeukset;
+
+    }
+
+
 }
