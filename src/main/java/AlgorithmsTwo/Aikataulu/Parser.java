4d1fc50b96093dde5388c7268e0c3876330fdcb5
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-04-24 22:42:04 +0300

    Parseria käytetään regexeillä

--- src/main/java/AlgorithmsTwo/Aikataulu/Parser.java
+++ src/main/java/AlgorithmsTwo/Aikataulu/Parser.java
@@ -1,7 +1,6 @@
 package AlgorithmsTwo.Aikataulu;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Parser {
     ArrayList<AikaTaulutus> aikataulutukset;
@@ -10,75 +9,26 @@ public class Parser {
         this.aikataulutukset = new ArrayList<>();
     }
 
+    //TODO: Refaktoroidaan tämä niin, että käyttää pelkkiä RegExpejä.
+    // nollat eivät ole tarpeellisia joten voidaan käyttää array.split metodia
+    // kolme nollaa jakaa syötteen ja siihen kuulumattoman osan
+    // kaksi nollaa jakaa Aikataulutukset
+    // yksi nolla jakaaa yksittäiset käyttäjät
+
     /**
      * Luodaan syotteesta ArrayList Tapauksista
      */
     public void kasitteleSyote(String syote) {
-        ArrayList<String> tapausSyotteet = erotteleTapaukset(syote);
-        luoAikataulutukset(tapausSyotteet);
+        syote = siivoaSyotteestaHanta(syote);
+        String regexp = "(0\\s*0\\s*0)";
+        String[] aikaTaulutusSyotteet = syote.split("\\s+");
+
     }
 
-    private ArrayList<String> erotteleTapaukset(String syote) {
-        ArrayList<String> tapausSyotteet = new ArrayList<>();
-        int nollienMaara =0 ;
-        StringBuilder sb = new StringBuilder();
-        Scanner scanner = new Scanner(syote);
-
-        while (scanner.hasNext()) {
-            String merkki = scanner.next();
-            if (!merkki.equals("0")) {
-                sb.append(merkki);
-                sb.append(" ");
-                nollienMaara = 0;
-            }
-            if (merkki.equals("0")){
-                nollienMaara++;
-            }
-            if (nollienMaara == 2) {
-                tapausSyotteet.add(sb.toString());
-                sb = new StringBuilder();
-            }
-            if (nollienMaara == 3) {
-                break;
-            }
-
-        }
-        return tapausSyotteet;
+    private void siivoaSyotteestaHanta(final String syote) {
+        String re
+        syote.split()
     }
-
-    private void luoAikataulutukset(ArrayList<String> tapausMerkkijonot) {
-       for (String merkkiJono : tapausMerkkijonot) {
-           luoAikaTaulutus(merkkiJono);
-       }
-    }
-
-
-    private void luoAikaTaulutus(String merkkiJono) {
-        AikaTaulutus aikaTaulutus = new AikaTaulutus();
-        Scanner scanner = new Scanner(merkkiJono);
-        int kayttajanID = 1;
-        ArrayList<String> kayttajanToiveet = new ArrayList<>();
-        int nollienMaara = 0;
-
-        while (scanner.hasNext()) {
-            String merkki = scanner.next();
-            if (merkki.equals("0")) {
-                nollienMaara++;
-                if (nollienMaara ==1) {
-                    Kayttaja uusiKayttaja = new Kayttaja(kayttajanID, kayttajanToiveet);
-                    aikaTaulutus.lisaaKayttaja(uusiKayttaja);
-                    kayttajanID++;
-                    kayttajanToiveet = new ArrayList<>();
-                } else {
-                    return;
-                }
-            }else {
-                kayttajanToiveet.add(merkki);
-            }
-        }
-        aikataulutukset.add(aikaTaulutus);
-    }
-
 
     public void tulostaAikataulutukset() {
         System.out.println("Tulostetaan aikataulutukset");
