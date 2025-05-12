043d9428982d3520ca820ee6c45d7dd4220453e8
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-05-12 10:54:00 +0200

    Fiksailtu tulostuksia

--- src/main/java/AlgorithmsTwo/Esitieto/Kurssi.java
+++ src/main/java/AlgorithmsTwo/Esitieto/Kurssi.java
@@ -46,7 +46,7 @@ public class Kurssi implements Comparable<Kurssi>{
     }
 
     public void tulosta() {
-        System.out.printf("%d %s %d ", id, nimi, periodi);
+        System.out.printf("%d: %d %s ", periodi, id, nimi);
         System.out.println();
     }
 
