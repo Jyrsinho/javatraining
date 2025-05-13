4e8a847f2267ddfd42963f1da053fa845447862d
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2025-05-13 15:12:36 +0200

    Parseria refaktoroitu

--- src/main/java/AlgorithmsTwo/Esitieto/Parser.java
+++ src/main/java/AlgorithmsTwo/Esitieto/Parser.java
@@ -14,11 +14,73 @@ public class Parser {
      * @return {ArrayList<Kurssilista>} kaikki kayttajan syotteen Kurssilistat
      */
     public ArrayList<KurssiLista> kasitteleSyote() {
+        // 1 nolla lopettaa kurssin syotteen
+        // 2 nolla lopettaa Kurssilistan syotteen
+        // 3 nolla lopettaa koko syotteen
+        // Kurssin syote tulee jarjestyksessa id nimi periodi : ennakkotiedot
+
         this.scanner = new Scanner(System.in);
+        ArrayList<KurssiLista> kurssiListat = new ArrayList<>();
+        KurssiLista kurssiLista = new KurssiLista();
+        int nollaLaskuri = 0;
+        StringBuilder sb = new StringBuilder();
 
         while (scanner.hasNext()) {
             String seuraava = scanner.next();
+            System.out.println("Luettiin " +seuraava);
+            if (seuraava.equals("0")) {
+                nollaLaskuri++;
+                System.out.println("Lisattiin nollalaskuri. Arvo nyt " +nollaLaskuri);
+            } else {
+                nollaLaskuri = 0;
+                System.out.println("Nollalaskuri nollattu");
+            }
+
+            switch (nollaLaskuri) {
+                case 0:
+                    System.out.println("Nollalaskuri on 0. Lisätään stringbuilderiin");
+                    sb.append(seuraava);
+                    sb.append(" ");
+                    System.out.println("sb:ssä nyt: " + sb);
+                    continue;
+                case 1:
+                    Kurssi uusiKurssi = luoKurssiSyotteesta(sb.toString());
+                    sb = new StringBuilder();
+                    System.out.println("Luotiin uusi Kurssi >" );
+                    uusiKurssi.tulosta();
+                    kurssiLista.lisaaKurssi(uusiKurssi);
+                    continue;
+                case 2:
+                    kurssiListat.add(kurssiLista);
+                    System.out.println("Lisättiin kurssilistoihin ->");
+                    kurssiLista.tulosta();
+                    kurssiLista = new KurssiLista();
+                    continue;
+                default:
+                    System.out.println("Kolme nollaa. Lopetetaan.");
+                    break;
+            }
         }
-        return new ArrayList<KurssiLista>();
+        return kurssiListat;
+    }
+
+    private Kurssi luoKurssiSyotteesta(String s) {
+        ArrayList<Integer> ennakkotiedot = new ArrayList<>();
+        Scanner scanner = new Scanner(s);
+        int id = scanner.nextInt();
+        String nimi = scanner.next();
+        String periodi = scanner.next();
+        String siivottuperiodi = periodi.replaceAll(":", "");
+        int periodiInteger = Integer.parseInt(siivottuperiodi);
+
+        while (scanner.hasNext()) {
+            try {
+                int ennakkotieto = Integer.parseInt(scanner.next());
+                ennakkotiedot.add(ennakkotieto);
+            } catch (Exception e) {
+                continue;
+            }
+        }
+       return new Kurssi(id, nimi, periodiInteger, ennakkotiedot);
     }
 }
